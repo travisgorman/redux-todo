89f95c1f0f58f9007c4f80e0b956e449d61c2959
Travis Gorman <thefarbetween@me.com>
2017-05-04 15:32:38 -0500

    update readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-// import { createStore } from 'redux'
-// import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 import store from './index.js'
+import { connect } from 'react-redux'
+const { number, func } = React.PropTypes
 
-
-const App = React.createClass({
-  onDecrement () {
-    store.dispatch({type: 'DECREMENT'})
-  },
-  onIncrement () {
-    store.dispatch({type: 'INCREMENT'})
-  },
-  render () {
+const Counter = React.createClass({
+  propTypes: {value: number, onIncrement: func, onDecrement: func},
+  render() {
     return (
-      <div>
-        <h1>{value}</h1>
-        <button onClick={this.onDecrement}>-</button>
-        <button onClick={this.onIncrement}>+</button>
+      <div className='counter'>
+        <h1>{this.props.value}</h1>
+        <button onClick={this.props.onDecrement}>-</button>
+        <button onClick={this.props.onIncrement}>+</button>
       </div>
     )
   }
 })
 
-export default App;
+const mapStateToProps(state, props) {
+  return {
+    value: state.value
+  }
+}
+export default connect(mapStateToProps)(Counter)
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore } from 'redux'
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
+const { number, func } = React.PropTypes
 // root reducer
 const counter = (state = 0, action) => {
   switch(action.type) {
@@ -17,31 +17,50 @@ const counter = (state = 0, action) => {
 }
 
 // stateless functional display component
-const Counter = (props) => (
-  <div className='counter'>
-    <h1>{props.value}</h1>
-    <button onClick={props.onDecrement}>-</button>
-    <button onClick={props.onIncrement}>+</button>
-  </div>
-)
+// const Counter = (props) => ({
+//   value: props.value,
+//   onIncrement () {
+//     return store.dispatch({type: 'INCREMENT'})
+//   },
+//   onDecrement () {
+//     return store.dispatch({type: 'DECREMENT'})
+//   },
+//   render () {
+//     return (
+//       <div className='counter'>
+//         <h1>{props.value}</h1>
+//         <button onClick={props.onDecrement}>-</button>
+//         <button onClick={props.onIncrement}>+</button>
+//       </div>
+//     )
+//   }
+// })
+
+const Counter = React.createClass({
+  propTypes: {
+    value: number,
+    onIncrement: func,
+    onDecrement: func
+  },
+  render() {
+    return (
+      <div className='counter'>
+        <h1>{this.props.value}</h1>
+        <button onClick={this.props.onDecrement}>-</button>
+        <button onClick={this.props.onIncrement}>+</button>
+      </div>
+    )
+  }
+})
 
 // the Redux store
 const store = createStore(counter, devTools)
 
-// the render function
-const render = () => {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => store.dispatch({type: 'INCREMENT'})}
-      onDecrement={() => store.dispatch({type: 'DECREMENT'})}
-    />,
-    document.getElementById('counter')
-  )
-}
-
-// subscribe to the store
-store.subscribe(render)
-
-// call the render function
-render()
+ReactDOM.render(
+  <Counter
+    value={store.getState()}
+    onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+    onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+  />,
+  document.getElementById('counter')
+)
