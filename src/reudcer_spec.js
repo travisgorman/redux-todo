2da9433fe7d5bbfe5311e45350fee23b0dca3531
Travis Gorman <thefarbetween@me.com>
2017-05-06 10:40:36 -0500

    addCounter, removeCounter, incrementCounter

--- src/reudcer_spec.js
+++ src/reudcer_spec.js
@@ -21,23 +21,50 @@ expect(counter(1, { type: 'DECREMENT' })).toEqual(0)
 expect(counter(1, { type: 'SOMETHING_ELSE' })).toEqual(1)
 
 const addCounter = (list) => {
-  list.push(0)
-  return list
+  return list.concat([0])
 }
 
-const addCounter = (list) => {
-  return list.concat([0])
+const removeCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index + 1)
+  ]
+}
+
+const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ]
 }
 
 deepFreeze(listBefore)
 
-const testAddCounter = () => {
-  const listBefore = []
-  const listAfter = [0]
-  expect(
-    addCounter(listBefore)
-  ).toEqual(listAfter)
-}
+// const testAddCounter = () => {
+//   const listBefore = []
+//   const listAfter = [0]
+//   expect(
+//     addCounter(listBefore)
+//   ).toEqual(listAfter)
+// }
+
+// const testRemoveCounter = () => {
+//   const listBefore = [0, 10, 20]
+//   const listAfter = [0, 20]
+
+//     expect(
+//     removeCounter(listBefore, 1)
+//   ).toEqual(listAfter)
+// }
+
+// const testIncrementCounter = () => {
+//   const listBefore = [0, 10, 20]
+//   const listAfter = [0, 11, 20]
+
+//   expect(
+//     incrementCounter(listBefore, 1)
+//   ).toEqual(listAfter)
+// }
 
 testAddCounter()
-console.log('all tests passed')
