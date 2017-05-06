629448d10f5cdf07d66254fa38ab0b9e4b199aaa
Travis Gorman <thefarbetween@me.com>
2017-05-06 09:49:28 -0500

    refactor with ES6 syntax

--- src/reudcer_spec.js
+++ src/reudcer_spec.js
@@ -1,16 +1,14 @@
 import counter from './reducers'
 import expect from 'expect'
 
-function counter(state, action) {
-  if (typeof state === 'undefined') {
-    return 0
-  }
-  if (action.type === 'INCREMENT') {
-    return state + 1
-  } else if (action.type === 'DECREMENT') {
-    return state - 1
-  } else {
-    return state
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
   }
 }
 
@@ -21,8 +19,3 @@ expect(counter(2, { type: 'DECREMENT' })).toEqual(1)
 expect(counter(1, { type: 'DECREMENT' })).toEqual(0)
 
 expect(counter(1, { type: 'SOMETHING_ELSE' })).toEqual(1)
-
-
-console.log('Current state:');
-console.log(store.getState());
-console.log('-----------------');
