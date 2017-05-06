c6489833b0a212b222c53e360a35a5a53bc534a8
Travis Gorman <thefarbetween@me.com>
2017-05-06 10:06:49 -0500

    addCounter test

--- src/reudcer_spec.js
+++ src/reudcer_spec.js
@@ -19,3 +19,25 @@ expect(counter(2, { type: 'DECREMENT' })).toEqual(1)
 expect(counter(1, { type: 'DECREMENT' })).toEqual(0)
 
 expect(counter(1, { type: 'SOMETHING_ELSE' })).toEqual(1)
+
+const addCounter = (list) => {
+  list.push(0)
+  return list
+}
+
+const addCounter = (list) => {
+  return list.concat([0])
+}
+
+deepFreeze(listBefore)
+
+const testAddCounter = () => {
+  const listBefore = []
+  const listAfter = [0]
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter)
+}
+
+testAddCounter()
+console.log('all tests passed')
