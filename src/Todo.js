59ca3c521f32b6c50f91ad409297c7a349fe34cb
Travis Gorman <thefarbetween@me.com>
2017-05-06 11:04:11 -0500

    todo reducer function and test

--- src/Todo.js
+++ src/Todo.js
@@ -1,27 +1,37 @@
-  // flips the `completed` value to its opposite
-const toggleTodo = (todo) => {
-  // return Object.assign({}, todo, {
-  //   completed: !todo.completed
-  // })
-  return {
-    ...todo,
-    completed: !todo.completed
+// writing a Todo list reducer -- adding a todo
+const todos = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.text,
+          completed: false
+        }
+      ]
+      default:
+        return state
   }
 }
-
-const testToggleTodo = () => {
-  const before = {
+// testing the todo reducer
+const testAddTodo = () => {
+  // state before
+  const before = []
+  // action being dispatched
+  const action = {
+    type: 'ADD_TODO',
     id: 0,
-    text: 'Learn Redux',
-    completed: false
+    text: 'Learn Redux'
   }
-  const after = {
-    id: 0,
-    text: 'Learn Redux',
-    completed: true
-  }
+  const after = [
+    { id: 0, text: 'Learn Redux', completed: false }
+  ]
+  // freeze state and action
+  deepFreeze(before)
+  deepFreeze(action)
+  // verify that our reduce works
   expect(
-    toggleTodo(before)
+    todos(before, action)
   ).toEqual(after)
 }
-testToggleTodo()
