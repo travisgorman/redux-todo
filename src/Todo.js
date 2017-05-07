89cddd6b3bb84d7fb71977ac271b95a6ab308a94
Travis Gorman <thefarbetween@me.com>
2017-05-07 11:43:28 -0500

    reducer composition with arrays

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,25 @@
-
-
-
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {id: action.id, text: action.text, completed: false}
+    case 'TOGGLE_TODO':
+      return (state.id !== action.id)
+        ? state
+        : { ...state, completed: !state.completed }
+    default:
+      return state
+  }
+}
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return todo(undefined, action)
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action))
+    default:
+      return state
+  }
+}
 // switch case with ternary
   // case: 'TOGGLE_TODO':
   //   return [
