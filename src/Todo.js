549f40a7e4ee59144eda642183c98dd97a4bdff9
Travis Gorman <thefarbetween@me.com>
2017-05-07 11:28:34 -0500

    drill reducer composition

--- src/Todo.js
+++ src/Todo.js
@@ -1,38 +1,5 @@
-// The todo reducer handles the individual todo
-// the state refers to the values of the todo object, NOT the entire array of todos
-const todo = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      }
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state
-      }
-      return {...state, completed: !state.completed}
-    default:
-      return state
-  }
-}
 
-// The todos reducer handles the entire array of todo objects
-// here state refers to the array - not each individual object
-const todos = (state = [], action) => {
-  switch(action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ]
-    case: 'TOGGLE_TODO':
-      return state.map(t => todo(t, action))
-    default:
-      return state
-  }
-}
+
 
 // switch case with ternary
   // case: 'TOGGLE_TODO':
