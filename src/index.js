ba84c30551841d0138032bb56447c598946ce3bd
Travis Gorman <thefarbetween@me.com>
2017-05-08 17:06:53 -0500

    renders input value

--- src/index.js
+++ src/index.js
@@ -97,10 +97,13 @@ class TodoApp extends React.Component {
   render () {
     return (
       <div>
+        <input ref={node => {
+          this.input = node
+        }} />
         <button onClick={() => {
           store.dispatch({
             type: 'ADD_TODO',
-            text: 'Test',
+            text: this.input.value,
             id: nextTodoId++
           });
         }}>
