e3eb055d9055b81dade4bc5d1c7fe5b37b9f6187
Travis Gorman <thefarbetween@me.com>
2017-05-08 14:22:15 -0500

    visibilityFilter

--- src/index.js
+++ src/index.js
@@ -44,9 +44,9 @@ const todos = (state = [], action) => {
 const visibilityFilter = (state = 'SHOW_ALL', action) => {
   switch (action.type) {
     case 'SET_VISIBILITY_FILTER':
-      return action.filter;
+      return action.filter
     default:
-      return state;
+      return state
   }
 }
 
