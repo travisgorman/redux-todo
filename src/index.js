ddc6bc2155d69e0a562f7b5e29fde600987f44ee
Travis Gorman <thefarbetween@me.com>
2017-05-08 13:15:41 -0500

    todoApp uses reducer composition to build an object

--- src/index.js
+++ src/index.js
@@ -41,8 +41,30 @@ const todos = (state = [], action) => {
   }
 }
 
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(
+      state.todos,
+      action
+    ),
+    visibilityFilter: visibilityFilter(
+      state.visibilityFilter,
+      action
+    )
+  }
+}
+
 // const { createStore } = Redux;
-const store = createStore(todos)
+const store = createStore(todoApp)
 
 console.log( 'initial state:' )
 console.log( store.getState() )
