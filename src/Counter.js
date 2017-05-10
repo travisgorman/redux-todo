fa5347e4beca0d1c1001f39b041b6983d7752502
Travis Gorman <thefarbetween@me.com>
2017-05-10 18:10:28 -0500

    visibilityFilter shows all, completed or active

---
+++ src/Counter.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { createStore } from 'redux'
+import expect from 'expect'
+import deepFreeze from 'deep-freeze'
+// root reducer
+// declares a default state of 0, and takes an action argument
+// switch statement that returns a new state depending on the type of action
+const counter = function(state = 0, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+// stateless functional display component
+// displays the value and shows two buttons
+// each button is given an event handler that will be passed in on render
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div className='counter'>
+    <h1>{value}</h1>
+    <button onClick={onDecrement}>-</button>
+    <button onClick={onIncrement}>+</button>
+  </div>
+)
+
+// this connects the store to the redux devtools
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+// the Redux store
+// uses createStore from Redux
+// takes the reducer (counter)
+// and connects to the devTools in browser
+const store = createStore(counter, devTools);﻿
+
+// render function mounts the Counter component to the DOM 'counter' element
+// gets value from the store with store.getState()
+// passes onIncrement and onDecrement props
+// each dispatch an action to the store
+const render = () => {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({type: 'INCREMENT',})}
+      onDecrement={() => store.dispatch({type: 'DECREMENT',})}
+    />,
+      document.getElementById('counter')
+  )
+}
+
+// subscribe to the store, passing in the render function
+store.subscribe(render)
+// call the render function
+render()
+
+/*
--- src/index.js
+++ src/index.js
@@ -61,11 +61,47 @@ const todoApp = combineReducers({
 // the Redux store
 const store = createStore(todoApp)
 
+const FilterLink = ({
+  filter,
+  children
+}) => {
+  return (
+    <a href='#'
+      onClick={e => {
+        e.preventDefault()
+        store.dispatch({
+          type: 'SET_VISIBILITY_FILTER',
+          filter
+        })
+      }}
+    >
+      {children}
+    </a>
+  )
+}
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos
+    case 'SHOW_ACTIVE':
+      return todos.filter(t => !t.completed)
+    case 'SHOW_COMPLETED':
+      return todos.filter(t => t.completed)
+    default:
+      return todos
+  }
+}
+
 let nextTodoId = 0;
 
 // View Layer - App Component
 class TodoApp extends React.Component {
   render () {
+    const visibleTodos = getVisibleTodos(
+      this.props.todos,
+      this.props.visibilityFilter
+    )
     return (
       <div>
         <input ref={node => {
@@ -77,13 +113,12 @@ class TodoApp extends React.Component {
             text: this.input.value,
             id: nextTodoId++
           });
-          console.log('store.todos:', store.getState().todos )
           this.input.value = ''
         }}>
           Add Todo
         </button>
         <ul>
-          {this.props.todos.map(todo =>
+          {visibleTodos.map(todo =>
             <li key={todo.id}
               onClick={() => {
                 store.dispatch({
@@ -97,15 +132,32 @@ class TodoApp extends React.Component {
             </li>
           )}
         </ul>
+        <p>
+          Show:
+          {'  '}
+          <FilterLink filter='SHOW_ALL'>
+            All
+          </FilterLink>
+          {'  '}
+          <FilterLink filter='SHOW_ACTIVE'>
+            Active
+          </FilterLink>
+          {'  '}
+          <FilterLink filter='SHOW_COMPLETED'>
+            Completed
+          </FilterLink>
+        </p>
       </div>
     )
   }
 }
 
+
+
 const render = () => {
   ReactDOM.render(
     <TodoApp
-      todos={store.getState().todos} />,
+      {...store.getState()} />,
     document.getElementById('todo')
   )
 }
