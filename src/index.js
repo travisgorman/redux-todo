1a8e77be581434217e4f2c1de3a63d1498ce03bb
Travis Gorman <thefarbetween@me.com>
2017-05-13 21:05:28 -0500

    extract FilterLink component

--- src/index.js
+++ src/index.js
@@ -192,13 +192,15 @@ const FilterLink = ({
   onClick,
   children,
 }) => {
+  if (filter === currentFilter) {
+    return <span>{children}</span>
+  }
   return (
     <a href='#'
       onClick={e => {
         e.preventDefault()
         onClick(filter)
-      }}
-    >
+      }}>
       {children}
     </a>
   )
